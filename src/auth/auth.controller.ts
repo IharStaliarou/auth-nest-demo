77457d434974eb691e149e953e9afaa82da21adb
IHAR STALIAROU <ihar.staliarou.dev@gmail.com>
2025-09-26 15:29:31 +0300

    feat: Added a basic description of methods for swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,11 +11,27 @@ import { AuthService } from './auth.service';
 import { RegisterRequest } from './dto/register.dto';
 import { LoginRequest } from './dto/login.dto';
 import { type Request, type Response } from 'express';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthResponse } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'User registration',
+    description: 'Creating a new user and adding the account to the database',
+  })
+  @ApiOkResponse({ type: AuthResponse })
+  @ApiBadRequestResponse({ description: 'Uncorrect email or password' })
+  @ApiConflictResponse({ description: 'User with this email already exists' })
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   async register(
@@ -25,6 +41,13 @@ export class AuthController {
     return this.authService.register(res, dto);
   }
 
+  @ApiOkResponse({ type: AuthResponse })
+  @ApiBadRequestResponse({ description: 'Uncorrect email or password' })
+  @ApiNotFoundResponse({ description: 'User with this email not found' })
+  @ApiOperation({
+    summary: 'User login',
+    description: 'Authorizes the user and issues an access token',
+  })
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(
@@ -34,12 +57,21 @@ export class AuthController {
     return this.authService.login(res, dto);
   }
 
+  @ApiOperation({
+    summary: 'Logout from system',
+  })
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   async logout(@Res({ passthrough: true }) res: Response) {
     return this.authService.logout(res);
   }
 
+  @ApiOperation({
+    summary: 'Update access token',
+    description: 'Generates a new access token',
+  })
+  @ApiOkResponse({ type: AuthResponse })
+  @ApiUnauthorizedResponse({ description: 'Invalid refresh token' })
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   async refresh(
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsNotEmpty,
@@ -7,11 +8,21 @@ import {
 } from 'class-validator';
 
 export class LoginRequest {
+  @ApiProperty({
+    description: 'Email of user',
+    example: 'j.doe@example.com',
+  })
   @IsString({ message: 'Email must be a string' })
   @IsNotEmpty({ message: 'Email is required' })
   @IsEmail({}, { message: 'Uncorrect email format' })
   email: string;
 
+  @ApiProperty({
+    description: 'Password of user account',
+    example: 'Qvd5tmoE9e1_33',
+    minLength: 8,
+    maxLength: 128,
+  })
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password is required' })
   @MinLength(8, { message: 'Password must be at least 8 characters' })
